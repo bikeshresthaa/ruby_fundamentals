6cedbc01da85a491ce86fe7c2c03298abdf2f7ff
bikeshresthaa <eshbikesh@gmail.com>
2025-12-22 15:41:50 +0545

    Create hash for 5 countries and their data

---
+++ hash_assignmet.rb
@@ -0,0 +1,26 @@
+countries = {
+  "Nepal" => { 
+    "Population" => 20000000, 
+    "Gdp per capita ($)" => 1447.31, 
+    "Capital" => "Kathmandu"},
+  "India" => {
+    "Population" => 140000000, 
+    "Gdp per capita ($)" => 2696.66, 
+    "Capital" => "New Delhi"
+  },
+  "China" => {
+    "Population" => 140900000, 
+    "Gdp per capita ($)" => 13303.15, 
+    "Capital" => "Beijing"
+  },
+  "Russia" => {
+    "Population" => 143000000, 
+    "Gdp per capita ($)" => 14889.02, 
+    "Capital" => "Moscow"
+  },
+  "Pakistan" => {
+    "Population" => 251000000, 
+    "Gdp per capita ($)" => 1484.75, 
+    "Capital" => "Islamabad"
+  }
+}
